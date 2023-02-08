bb99a0303983f96b92aa876c1dbfb7bd59fa90b2
Ian Namie <Iannamie82@gmail.com>
2023-02-08 16:13:45 -0600

    finished exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -2,9 +2,43 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            var favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Ew thats gross I hate math!");
+                    break;
+
+                case "english":
+                    Console.WriteLine("Hey I really like english too, thats awesome.");
+                    break;
+
+                case "science":
+                    Console.WriteLine("I really enjoy Science too! My favorite type of science is Chemistry.");
+                    break;
+
+                case "history":
+                    Console.WriteLine("I always really liked my history teachers, most of them also coached a sport");
+                    break;
+
+                case "band":
+                    Console.WriteLine("Band was a lot of fun but I wasn't a huge fan of our conductor!");
+                    break;
+
+                default: 
+                    Console.WriteLine("ohh that sounds like a lot of fun you'll have to tell me more about it!");
+                    break;
+            }
+
+
+
+
+
+
         }
     }
 }
